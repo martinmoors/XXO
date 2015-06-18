2e478dc66178064d08a5e80c8c3f1529741c27a1
Martin Moors <martin.moors@fpz.de>
2015-06-18 16:24:24 +0200

    project initial coimmit

---
+++ XXO/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace XXO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.box1 = new System.Windows.Forms.PictureBox();
+            this.box2 = new System.Windows.Forms.PictureBox();
+            this.box3 = new System.Windows.Forms.PictureBox();
+            this.box4 = new System.Windows.Forms.PictureBox();
+            this.box5 = new System.Windows.Forms.PictureBox();
+            this.box6 = new System.Windows.Forms.PictureBox();
+            this.box7 = new System.Windows.Forms.PictureBox();
+            this.box8 = new System.Windows.Forms.PictureBox();
+            this.box9 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.box1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // box1
+            // 
+            this.box1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.box1.Location = new System.Drawing.Point(22, 13);
+            this.box1.Name = "box1";
+            this.box1.Size = new System.Drawing.Size(150, 150);
+            this.box1.TabIndex = 0;
+            this.box1.TabStop = false;
+            this.box1.Click += new System.EventHandler(this.box1_Click);
+            // 
+            // box2
+            // 
+            this.box2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.box2.Location = new System.Drawing.Point(225, 13);
+            this.box2.Name = "box2";
+            this.box2.Size = new System.Drawing.Size(150, 150);
+            this.box2.TabIndex = 1;
+            this.box2.TabStop = false;
+            this.box2.Click += new System.EventHandler(this.box2_Click);
+            // 
+            // box3
+            // 
+            this.box3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.box3.Location = new System.Drawing.Point(420, 13);
+            this.box3.Name = "box3";
+            this.box3.Size = new System.Drawing.Size(150, 150);
+            this.box3.TabIndex = 2;
+            this.box3.TabStop = false;
+            this.box3.Click += new System.EventHandler(this.box3_Click);
+            // 
+            // box4
+            // 
+            this.box4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.box4.Location = new System.Drawing.Point(22, 197);
+            this.box4.Name = "box4";
+            this.box4.Size = new System.Drawing.Size(150, 150);
+            this.box4.TabIndex = 3;
+            this.box4.TabStop = false;
+            this.box4.Click += new System.EventHandler(this.box4_Click);
+            // 
+            // box5
+            // 
+            this.box5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.box5.Location = new System.Drawing.Point(225, 197);
+            this.box5.Name = "box5";
+            this.box5.Size = new System.Drawing.Size(150, 150);
+            this.box5.TabIndex = 4;
+            this.box5.TabStop = false;
+            this.box5.Click += new System.EventHandler(this.box5_Click);
+            // 
+            // box6
+            // 
+            this.box6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.box6.Location = new System.Drawing.Point(420, 197);
+            this.box6.Name = "box6";
+            this.box6.Size = new System.Drawing.Size(150, 150);
+            this.box6.TabIndex = 5;
+            this.box6.TabStop = false;
+            this.box6.Click += new System.EventHandler(this.box6_Click);
+            // 
+            // box7
+            // 
+            this.box7.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.box7.Location = new System.Drawing.Point(22, 393);
+            this.box7.Name = "box7";
+            this.box7.Size = new System.Drawing.Size(150, 150);
+            this.box7.TabIndex = 6;
+            this.box7.TabStop = false;
+            this.box7.Click += new System.EventHandler(this.box7_Click);
+            // 
+            // box8
+            // 
+            this.box8.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.box8.Location = new System.Drawing.Point(225, 393);
+            this.box8.Name = "box8";
+            this.box8.Size = new System.Drawing.Size(150, 150);
+            this.box8.TabIndex = 7;
+            this.box8.TabStop = false;
+            this.box8.Click += new System.EventHandler(this.box8_Click);
+            // 
+            // box9
+            // 
+            this.box9.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.box9.Location = new System.Drawing.Point(420, 393);
+            this.box9.Name = "box9";
+            this.box9.Size = new System.Drawing.Size(150, 150);
+            this.box9.TabIndex = 8;
+            this.box9.TabStop = false;
+            this.box9.Click += new System.EventHandler(this.box9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 555);
+            this.Controls.Add(this.box9);
+            this.Controls.Add(this.box8);
+            this.Controls.Add(this.box7);
+            this.Controls.Add(this.box6);
+            this.Controls.Add(this.box5);
+            this.Controls.Add(this.box4);
+            this.Controls.Add(this.box3);
+            this.Controls.Add(this.box2);
+            this.Controls.Add(this.box1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.box1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box9)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox box1;
+        private System.Windows.Forms.PictureBox box2;
+        private System.Windows.Forms.PictureBox box3;
+        private System.Windows.Forms.PictureBox box4;
+        private System.Windows.Forms.PictureBox box5;
+        private System.Windows.Forms.PictureBox box6;
+        private System.Windows.Forms.PictureBox box7;
+        private System.Windows.Forms.PictureBox box8;
+        private System.Windows.Forms.PictureBox box9;
+    }
+}
+
---
+++ XXO/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XXO
+{
+    public partial class Form1 : Form
+    {
+
+        private Boolean flag = false;
+
+        private int winCounterPlayer1 = 1;
+
+        private int winCounterPlayer2 = 1;
+
+        private int[,] gameBoard = new int[3,3];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Draw grid
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // Create pen.
+            Pen pen = new Pen(Color.Black, 3);
+
+            // Create points that define the lines.
+            Point point1 = new Point(5, 175);
+            Point point2 = new Point(575, 175);
+            Point point3 = new Point(5, 375);
+            Point point4 = new Point(575, 375);
+            Point point5 = new Point(200, 5);
+            Point point6 = new Point(200, 550);
+            Point point7 = new Point(400, 5);
+            Point point8 = new Point(400, 550);
+
+            //Draw grid
+            e.Graphics.DrawLine(pen, point1, point2);
+            e.Graphics.DrawLine(pen, point3, point4);
+            e.Graphics.DrawLine(pen, point5, point6);
+            e.Graphics.DrawLine(pen, point7, point8);
+        }
+
+        private void box1_Click(object sender, EventArgs e)
+        {
+            if (flag == true)
+            {
+                this.box1.Image = Properties.Resources.O;
+                flag = false;
+                gameBoard[0, 0] = 0;
+            }
+            else
+            {
+                this.box1.Image = Properties.Resources.X;
+                flag = true;
+                gameBoard[0, 0] = 1;
+            }
+            box1.Enabled = false;
+        }
+
+        private void box2_Click(object sender, EventArgs e)
+        {
+            if (flag == true)
+            {
+                this.box2.Image = Properties.Resources.O;
+                flag = false;
+                gameBoard[0, 1] = 0;
+            }
+            else
+            {
+                this.box2.Image = Properties.Resources.X;
+                flag = true;
+                gameBoard[0, 1] = 1;
+            }
+            box2.Enabled = false;
+        }
+
+        private void box3_Click(object sender, EventArgs e)
+        {
+            if (flag == true)
+            {
+                this.box3.Image = Properties.Resources.O;
+                flag = false;
+                gameBoard[0, 2] = 0;
+            }
+            else
+            {
+                this.box3.Image = Properties.Resources.X;
+                flag = true;
+                gameBoard[0, 2] = 1;
+            }
+            box3.Enabled = false;
+        }
+
+        private void box4_Click(object sender, EventArgs e)
+        {
+            if (flag == true)
+            {
+                this.box4.Image = Properties.Resources.O;
+                flag = false;
+                gameBoard[1, 0] = 0;
+            }
+            else
+            {
+                this.box4.Image = Properties.Resources.X;
+                flag = true;
+                gameBoard[1, 0] = 1;
+            }
+            box4.Enabled = false;
+        }
+
+        private void box5_Click(object sender, EventArgs e)
+        {
+            if (flag == true)
+            {
+                this.box5.Image = Properties.Resources.O;
+                flag = false;
+                gameBoard[1, 1] = 0;
+            }
+            else
+            {
+                this.box5.Image = Properties.Resources.X;
+                flag = true;
+                gameBoard[1, 1] = 1;
+            }
+            box5.Enabled = false;
+        }
+
+        private void box6_Click(object sender, EventArgs e)
+        {
+            if (flag == true)
+            {
+                this.box6.Image = Properties.Resources.O;
+                flag = false;
+                gameBoard[1, 2] = 0;
+            }
+            else
+            {
+                this.box6.Image = Properties.Resources.X;
+                flag = true;
+                gameBoard[1, 2] = 1;
+            }
+            box6.Enabled = false;
+        }
+
+        private void box7_Click(object sender, EventArgs e)
+        {
+            if (flag == true)
+            {
+                this.box7.Image = Properties.Resources.O;
+                flag = false;
+                gameBoard[2, 0] = 0;
+            }
+            else
+            {
+                this.box7.Image = Properties.Resources.X;
+                flag = true;
+                gameBoard[2, 0] = 1;
+            }
+            box7.Enabled = false;
+        }
+
+        private void box8_Click(object sender, EventArgs e)
+        {
+            if (flag == true)
+            {
+                this.box8.Image = Properties.Resources.O;
+                flag = false;
+                gameBoard[2, 1] = 0;
+            }
+            else
+            {
+                this.box8.Image = Properties.Resources.X;
+                flag = true;
+                gameBoard[2, 1] = 1;
+            }
+            box8.Enabled = false;
+        }
+
+        private void box9_Click(object sender, EventArgs e)
+        {
+            if (flag == true)
+            {
+                this.box9.Image = Properties.Resources.O;
+                flag = false;
+                gameBoard[2, 2] = 0;
+            }
+            else
+            {
+                this.box9.Image = Properties.Resources.X;
+                flag = true;
+                gameBoard[2, 2] = 1;
+            }
+            box9.Enabled = false;
+        }
+    }
+}
